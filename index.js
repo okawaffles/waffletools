5ca17b8912aa66bc6b38eb70fbfc8e904ca8ee7d
Eli Clark <77750838+okawaffles@users.noreply.github.com>
2022-10-16 19:29:21 +0000

    fix it!

--- index.js
+++ index.js
@@ -12,17 +12,23 @@ app.get('/', (req, res) => {
 app.post('/mp3', async (req, res) => {
     let url = req.query.url;
 
-    await ytdl(url, 'audioonly').pipe(fs.createWriteStream(`./downloads/download.mp3`));
-
-    res.sendFile('./downloads/download.mp3');
+    await ytdl(url, { quality: 'audioonly' }).pipe(fs.createWriteStream(`./downloads/download.mp3`));
 });
 
 app.post('/mp4', async (req, res) => {
+    console.log(req.query);
     let url = req.query.url;
 
     await ytdl(url).pipe(fs.createWriteStream(`./downloads/download.mp4`));
 
-    res.sendFile('./downloads/download.mp4');
+    //res.sendFile('./downloads/download.mp4', {root: __dirname});
 });
+
+app.get('/dlmp3', (req, res) => {
+    res.sendFile('./downloads/download.mp3', {root: __dirname});
+})
+app.get('/dlmp4', (req, res) => {
+    res.sendFile('./downloads/download.mp4', {root: __dirname});
+})
 
 app.listen('4040');
