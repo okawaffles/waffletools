6503815434b74cf0b0cf6834959272b1d1a196d3
okawaffles <okawaffles@gmail.com>
2022-10-16 14:43:23 -0500

    gitignore pls work

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const express = require('express');
+const fmd = require('formidable');
 const app = express();
 
 app.set('view engine', 'ejs')
@@ -9,26 +10,13 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
-app.post('/mp3', async (req, res) => {
+app.post('/ytdl/mp3', (req, res) => {
     let url = req.query.url;
 
-    await ytdl(url, { quality: 'audioonly' }).pipe(fs.createWriteStream(`./downloads/download.mp3`));
+    ytdl(url, { filter: 'audioonly' }).pipe(fs.createWriteStream(`./downloads/download.mp3`).on('finish', () => {
+        res.sendFile('./downloads/download.mp3', {root:__dirname});
+        res.end();
+    }));
 });
-
-app.post('/mp4', async (req, res) => {
-    console.log(req.query);
-    let url = req.query.url;
-
-    await ytdl(url).pipe(fs.createWriteStream(`./downloads/download.mp4`));
-
-    //res.sendFile('./downloads/download.mp4', {root: __dirname});
-});
-
-app.get('/dlmp3', (req, res) => {
-    res.sendFile('./downloads/download.mp3', {root: __dirname});
-})
-app.get('/dlmp4', (req, res) => {
-    res.sendFile('./downloads/download.mp4', {root: __dirname});
-})
 
 app.listen('4040');
